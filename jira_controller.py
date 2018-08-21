73c2034dd000fa1267379d586dc62a61186242c4
Brian Cummings <brian.cummings@careerbuilder.com>
2018-08-21 07:57:17 -0400

    Updated the process to measure updates in hours versus days so that it can run multiple times per day

--- jira_controller.py
+++ jira_controller.py
@@ -1,4 +1,6 @@
 import datetime
+import pytz
+import math
 import get_jira_issues
 import jira_model
 import get_jira_worklog
@@ -14,30 +16,33 @@ handler.doRollover()
 logger.addHandler(handler)
 
 parser = argparse.ArgumentParser(__file__)
-parser.add_argument('-d',metavar='days', dest='days', help='Days to retrieve', type=int)
+parser.add_argument('-t',metavar='hours', dest='hours', help='Time in hours to retrieve', type=int)
 parser.add_argument('-i',metavar='issue key', dest='issue_key', help='Jira issue key', type=str)
 args = parser.parse_args()
-user_days = args.days
+user_hours = args.hours
 user_issue_key = args.issue_key
 
-curr_date = datetime.datetime.date(datetime.datetime.now())
-if user_days is None:
-    user_days = -1
+utc_datetime = pytz.utc.localize(datetime.datetime.utcnow())
+curr_datetime = utc_datetime.astimezone(pytz.timezone("America/New_York"))
+if user_hours is None:
+    user_hours = -1
     logger.info("Retrieving most recent updates from db")
-    last_updated = datetime.datetime.date(jira_model.return_last_update())
-    days_since_update = (curr_date - last_updated).days
+    last_updated = jira_model.return_last_update()
+    hours_since_update = math.ceil((curr_datetime - last_updated).total_seconds() / 3600)
+
 else:
-    logger.info("Received user input of {} days".format(user_days))
-    days_since_update = user_days
+    logger.info("Received user input of {} hours".format(user_hours))
+    hours_since_update = user_hours
 
 
-logger.info("Looking for updates in the past {} days".format(days_since_update))
+logger.info("Looking for updates in the past {} hours".format(hours_since_update))
 
 if user_issue_key is None:
-    if days_since_update > 0:
-        get_jira_issues.load_issues('updated >= -{}d'.format(days_since_update))
+    if hours_since_update > 0:
+        hours_since_update = hours_since_update
+        get_jira_issues.load_issues('updated >= -{}h'.format(hours_since_update))
 
-        issue_key_results = jira_model.return_keys('{} day'.format(days_since_update))
+        issue_key_results = jira_model.return_keys('{} hour'.format(hours_since_update))
 
         for i in issue_key_results:
             logger.info("Getting worklog for {}".format(i[0]))
