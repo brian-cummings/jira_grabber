d590e67fe64fcc4e32ddfb6f1505e6abed74608f
Brian Cummings <brian.cummings@careerbuilder.com>
2018-09-21 16:28:53 -0400

    Implemented batch email

--- send_email.py
+++ send_email.py
@@ -3,26 +3,35 @@ import configparser
 import jira_model
 import argparse
 import logging
+import json
 
 logger = logging.getLogger("jiraLogger")
 
-parser = argparse.ArgumentParser(__file__)
-parser.add_argument('-u', dest='user', help='User id', type=str)
-parser.add_argument('-n', dest='user_name', help='User name', type=str)
-parser.add_argument('-e', dest='email', help='User email', type=str)
-parser.add_argument('-d', dest='days', help='Days to query', type=int)
-args = parser.parse_args()
+#parser = argparse.ArgumentParser(__file__)
+#parser.add_argument('-u', dest='user', help='User id', type=str)
+#parser.add_argument('-n', dest='user_name', help='User name', type=str)
+#parser.add_argument('-e', dest='email', help='User email', type=str)
+#parser.add_argument('-d', dest='days', help='Days to query', type=int)
+#args = parser.parse_args()
 
 sp_config = configparser.ConfigParser()
 sp_config.read('config.ini')
 sp_apikey = sp_config['sparkpost']['key']
-sp = SparkPost(sp_apikey)
+sp_email = sp_config['sparkpost']['email']
+sp_noreply = sp_config['sparkpost']['noreply']
+sp_base_uri = sp_config['sparkpost']['base_uri']
+sp = SparkPost(sp_apikey, base_uri=sp_base_uri)
 
-def send_weekly_email(user,username,email,days):
+
+def send_recap_email(user,username,email,days):
 
     table = ""
     results = jira_model.return_worklogs(user, days)
     email_body = '<p>Hello ' + username + ',</p>'
+    if days == -1:
+        results_intro_text = '<p>This is your first email! Here are your results since you started tracking:</p>'
+    else:
+        results_intro_text = '<p>Here are your results for the past ' + str(days) + ' days:</p>'
     result_count = sum(1 for i in results)
     if result_count > 1:
         for date, amount in results:
@@ -31,9 +40,8 @@ def send_weekly_email(user,username,email,days):
                            + '</td><td align="center" valign="top">' + amount_str +'</td></tr>'
             table = table + final_string
 
-        email_body = email_body \
-            + '<p>Here are your results for the past ' + str(days) + ' days:</p>' \
-            '<table border="1" cellpadding="0" cellspacing="0" width="50%" >' \
+        email_body = email_body + results_intro_text \
+            + '<table border="1" cellpadding="0" cellspacing="0" width="50%" >' \
             '<tr> <th bgcolor="#D3D3D3">Date</th><th bgcolor="#D3D3D3">Hours</th>' \
             + table + '</table>'
     else:
@@ -45,16 +53,22 @@ def send_weekly_email(user,username,email,days):
                 'address': {
                     'email': email,
                     'name': username
-                }
+                },
+                "metadata" : {
+                    "binding" : "dedicated1"
+                  }
             },
         ],
         html=email_body,
-        from_email='Test User <test@sahmhill.com>',
+        from_email=sp_email,
         subject='Your ' + str(days) + ' day Jira Worklog',
         track_opens=True,
         track_clicks=True,
-        reply_to='no-reply@sparkpostmail.com'
+        reply_to=sp_noreply
     )
+    accepted_recipients = response['total_accepted_recipients']
+    if accepted_recipients >= 1:
+        jira_model.update_user_emailedate(user)
 
-if __name__ == "__main__":
-    send_weekly_email(args.user, args.user_name, args.email, args.days)
+#if __name__ == "__main__":
+#    send_recap_email(args.user, args.user_name, args.email, args.days)
